1f43490b2dd499dc36b98893b9b30dc5919f1c4f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-07 15:01:42 +0300

    Added some styling and functionality for a cross icon

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { XCircleIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { type ElementRef, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
@@ -24,6 +25,10 @@ export function Modal({ children }: { children: React.ReactNode }) {
       className="flex h-screen w-screen items-center justify-center rounded-xl bg-neutral-600/50"
       onClose={onDismiss}
     >
+      <XCircleIcon
+        className="absolute right-4 top-4 h-6 w-6 cursor-pointer text-foreground text-white"
+        onClick={onDismiss}
+      />
       {children}
       <button onClick={onDismiss} className="close-button" />
     </dialog>,
