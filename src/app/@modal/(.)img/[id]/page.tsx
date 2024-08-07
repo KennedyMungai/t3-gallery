66216369d5a34482b4ab3cbebb2337d104857024
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-07 15:17:15 +0300

    Added some info and styling to the image modal

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -17,7 +17,16 @@ const PhotoModal = async ({ params: { id: photoId } }: Props) => {
 
   return (
     <Modal>
-      <Image src={image.url} width={200} height={200} alt={image.name} />
+      <div className="flex gap-x-4">
+        <div className="flex-1">
+          <Image src={image.url} width={200} height={200} alt={image.name} />
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center text-2xl font-semibold text-white">
+            {image.name}
+          </p>
+        </div>
+      </div>
     </Modal>
   );
 };
