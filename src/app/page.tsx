5bbd347a0ad309cd708f707f9ee12e0e7c2c0147
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-07 11:15:07 +0300

    Refactored the authentication of the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { currentUser } from "@clerk/nextjs/server";
 import { getImages } from "~/server/queries";
 import Gallery from "./_components/gallery";
 
 export const dynamic = "force-dynamic";
 
 const HomePage = async () => {
-  const user = await currentUser();
-
   const images = await getImages();
 
   return (
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,7 +1,14 @@
 import "server-only";
 import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
 
-export const getImages = async () =>
-  await db.query.images.findMany({
+const user = auth();
+
+export const getImages = async () => {
+  if (!user.userId) throw new Error("Unauthorized");
+
+  return await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
     orderBy: (model, { desc }) => desc(model.id),
   });
+};
