0fa18926c0db26a4ca768ad3ca20eaa3885c81d1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-07 14:19:57 +0300

    Added some configurattion for the specific image

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
+import { getMyImage } from "~/server/queries";
+
 type Props = {
   params: {
     id: string;
   };
 };
 
-const ImagePage = ({ params: { id: photoId } }: Props) => {
-  return <div>{photoId}</div>;
+const ImagePage = async ({ params: { id: photoId } }: Props) => {
+  const image = await getMyImage(Number(photoId));
+
+  return (
+    <div className="flex h-full items-center justify-center">
+      <Image src={image.url} alt={image.name} width={200} height={200} />
+    </div>
+  );
 };
 
 export default ImagePage;
