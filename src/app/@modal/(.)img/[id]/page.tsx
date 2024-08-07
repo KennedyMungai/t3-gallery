ed7f7367eca600183d7c36c4a732538592d94593
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-07 14:33:13 +0300

    Parallel route working

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
+import { getMyImage } from "~/server/queries";
+
 type Props = {
   params: {
     id: string;
   };
 };
 
-const PhotoModal = ({ params: { id: photoId } }: Props) => {
-  return <div>{photoId}</div>;
+const PhotoModal = async ({ params: { id: photoId } }: Props) => {
+  const photoIdNum = Number(photoId);
+
+  if (Number.isNaN(photoIdNum)) throw new Error("Invalid photo Id");
+
+  const image = await getMyImage(photoIdNum);
+
+  return <Image src={image.url} width={120} height={120} alt={image.name} />;
 };
 
 export default PhotoModal;
