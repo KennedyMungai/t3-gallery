8fea7ba5b106960328a257aa92dd1ee837dbf725
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-07 14:53:24 +0300

    Added some styles to the modal

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { type ElementRef, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
+import { type ElementRef, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 export function Modal({ children }: { children: React.ReactNode }) {
@@ -19,12 +19,14 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-    <div className="modal-backdrop">
-      <dialog ref={dialogRef} className="modal" onClose={onDismiss}>
-        {children}
-        <button onClick={onDismiss} className="close-button" />
-      </dialog>
-    </div>,
+    <dialog
+      ref={dialogRef}
+      className="flex h-screen w-screen items-center justify-center rounded-xl bg-neutral-600/50"
+      onClose={onDismiss}
+    >
+      {children}
+      <button onClick={onDismiss} className="close-button" />
+    </dialog>,
     document.getElementById("modal-root")!,
   );
 }
